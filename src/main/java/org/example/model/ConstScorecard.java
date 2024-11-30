0841bebbb188499413c499c9414c345d96c0a332
Ryan N <ryann66@cs.washington.edu>
2024-11-29 17:27:51 -0800

    Work on scorecards

--- src/main/java/org/example/model/ConstScorecard.java
+++ src/main/java/org/example/model/ConstScorecard.java
@@ -1,7 +1,45 @@
 package org.example.model;
 
+import java.util.Map;
+
 /**
  * Const wrapper for scorecards
+ *
+ * Special meaning for integer values in scorecard
+ *  - negative numbers mean that the player does not have the card
+ *  - 0 means that the player must have the card
+ *  - positive numbers mean that the player might have the card
+ *  	Each positive number represents a group; the player must have
+ *  	at least one card in the group
  */
 public class ConstScorecard {
+	protected int[][] scorecard;
+
+	/**
+	 * Builds a comprehensive scorecard of everything the player knows
+	 *
+	 * Special meaning for integer values in scorecard
+	 *  - negative numbers mean that the player does not have the card
+	 *  - 0 means that the player must have the card
+	 *  - positive numbers mean that the player might have the card
+	 *  	Each positive number represents a group; the player must have
+	 *  	at least one card in the group
+	 *
+	 * @return a comprehensive scorecard
+	 */
+	public Map<Player, Map<Card.Value, Integer>> getScorecard() {
+		// TODO
+	}
+
+	/**
+	 * Builds a simple scorecard for what the player knows
+	 * Consists of one column that is either card found (true) or
+	 * card not found (false)
+	 * @return a simple one column scorecard where true represents
+	 * 		that a card has been found/eliminated and false represents
+	 * 		that the card could still be in the envelope
+	 */
+	public Map<Card.Value, Boolean> getSimpleScorecard() {
+		// TODO
+	}
 }
--- src/main/java/org/example/model/Player.java
+++ src/main/java/org/example/model/Player.java
@@ -4,4 +4,4 @@ package org.example.model;
  * Record for players
  * @param name the name of the player
  */
-public record Player(String name) { }
+public record Player(String name, int index) { }
