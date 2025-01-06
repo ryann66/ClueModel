3d92da74a4766f4a7c72d50545e6ee93bbb1a178
Ryan N <ryann66@cs.washington.edu>
2025-01-06 14:43:24 -0800

    ArrayScorecard implementation

--- src/main/java/org/example/model/ArrayScorecard.java
+++ src/main/java/org/example/model/ArrayScorecard.java
@@ -1,26 +1,98 @@
 package org.example.model;
 
+import java.util.AbstractMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+/**
+ * Array based implementation of Scorecard
+ */
 class ArrayScorecard implements Scorecard {
-	// todo
+	int idx = 0;
+
+	Player[] parr;
+
+	// Knowledge[PlayerID][Card Ordinal]
+	Knowledge[][] karr;
 
 	ArrayScorecard(PlayerList players) {
-		// todo
+		parr = new Player[players.getPlayerCount()];
+		karr = new Knowledge[parr.length][Card.NUM_CARDS];
 	}
 
 	@Override
 	public void mark(Player p, Card c, Knowledge k) {
-		// todo
+		int i = getPlayerIndex(p);
+		karr[i][c.ordinal()] = k;
 	}
 
 	@Override
 	public Knowledge get(Player p, Card c) {
-		// todo
-		return null;
+		return karr[getPlayerIndex(p)][c.ordinal()];
 	}
 
 	@Override
 	public PlayerScorecard get(Player p) {
-		// todo
-		return null;
+		return new ArrayPlayerScorecard(getPlayerIndex(p));
+	}
+
+	private int getPlayerIndex(Player p) {
+		if (parr[idx].equals(p)) return idx;
+		for (int i = idx + 1; i != idx; i++) {
+			if (i == parr.length) i = 0;
+			if (parr[i].equals(p)) {
+				// cache index
+				idx = i;
+				return idx;
+			}
+		}
+		throw new NoSuchElementException();
+	}
+
+	/**
+	 * Inner player card view accessor for ArrayScorecard
+	 */
+	private class ArrayPlayerScorecard implements PlayerScorecard {
+		int pidx;
+
+		ArrayPlayerScorecard(int playerIdx) {
+			this.pidx = playerIdx;
+		}
+
+		@Override
+		public Knowledge get(Card c) {
+			return null;
+		}
+
+		@Override
+		public void mark(Card c, Knowledge k) {
+
+		}
+
+		@Override
+		public Iterator<Map.Entry<Card, Knowledge>> iterator() {
+			return new CardIterator(pidx);
+		}
+	}
+
+	class CardIterator implements Iterator<Map.Entry<Card, Knowledge>> {
+		final int pidx;
+		int cidx = 0;
+
+		CardIterator(int playerIdx) {
+			pidx = playerIdx;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return cidx < Card.NUM_CARDS;
+		}
+
+		@Override
+		public Map.Entry<Card, Knowledge> next() {
+			Card c = Card.values()[cidx++];
+			return new AbstractMap.SimpleEntry<>(c, karr[pidx][cidx]);
+		}
 	}
 }
--- src/main/java/org/example/model/Scorecard.java
+++ src/main/java/org/example/model/Scorecard.java
@@ -1,15 +1,31 @@
 package org.example.model;
 
 interface Scorecard extends ImmutableScorecard {
+	/**
+	 * Marks the scorecard for the given player and card with knowledge k
+	 * @param p the player to mark
+	 * @param c the card to mark
+	 * @param k the knowledge to be marked
+	 */
 	void mark(Player p, Card c, Knowledge k);
 
-	Knowledge get(Player p, Card c);
-
+	/**
+	 * Gets a mutable view of the scorecard for a given player
+	 * @param p the player to get a scorecard for
+	 * @return a mutable view of the scorecard for a given player
+	 */
+	@Override
 	PlayerScorecard get(Player p);
 
+	/**
+	 * A mutable view of a player's scorecard
+	 */
 	interface PlayerScorecard extends ImmutablePlayerScorecard {
+		/**
+		 * Marks the player's scorecard for the given card with the given knowledge
+		 * @param c the card to mark
+		 * @param k the knowledge to mark
+		 */
 		void mark(Card c, Knowledge k);
-
-		public Knowledge get(Card c);
 	}
 }
