ca962d69c8d3242b9f35b8b0d1a13848fa44cf24
ryann66 <ryannelson6@hotmail.com>
2025-08-28 23:10:47 -0700

    Custom views added

---
+++ app/src/main/java/com/cluemodeler/ReorderableEditTextContainer.java
@@ -0,0 +1,36 @@
+package com.cluemodeler;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class ReorderableEditTextContainer extends ViewGroup {
+    public ReorderableEditTextContainer(Context context) {
+        super(context);
+    }
+
+    public ReorderableEditTextContainer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ReorderableEditTextContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ReorderableEditTextContainer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+        // args: box to layout inside of
+
+        // TODO: call layout() on children with specified bounds
+
+
+
+        // maybe use a constraint layout with unsatisfiable constraints?
+        // might allow for smoother animation
+        // onDragEvent()
+    }
+}
